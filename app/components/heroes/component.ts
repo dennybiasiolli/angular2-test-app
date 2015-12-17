18ae8cd4bc4211dd1aa849440dde03ab6560f5c8
Denny Biasiolli <denny.biasiolli@gmail.com>
2015-12-17 16:43:16 +0100

    Added heroes service

--- app/components/heroes/component.ts
+++ app/components/heroes/component.ts
@@ -1,29 +1,27 @@
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
+import {HeroService} from './service';
 import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
     selector: 'my-heroes',
     templateUrl: 'app/components/heroes/template.html',
     styleUrls: ['app/components/heroes/styles.css'],
-    directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
 export class HeroesComponent {
+    constructor(private _heroService: HeroService) {
+        this.getHeroes();
+    }
+    getHeroes() {
+        this.selectedHero = undefined;
+        this.heroes = [];
+        this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+        return this.heroes;
+    }
     public title = 'Tour of Heroes';
     public selectedHero: Hero;
-    public heroes = HEROES;
+    public heroes: Hero[];
     onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
-var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto" },
-    { "id": 14, "name": "Celeritas" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "RubberMan" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr IQ" },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Tornado" }
-];
